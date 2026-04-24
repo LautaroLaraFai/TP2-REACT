a49bcd900af2eedc819a7d96258b16d63787e6f5
Lautaro Lara <lautaronicolas.lara@est.fi.uncoma.edu.ar>
2026-04-24 02:01:56 -0300

    added icons and response example

--- src/services/getData.js
+++ src/services/getData.js
@@ -6,5 +6,27 @@ async function getData() {
     } catch (error) {
         console.error(error)
     }
-     
 }
+
+// response example
+// [
+//  {
+//     "id": 1,
+//     "Name": "Grand Theft Auto V",
+//     "Rating": 4,
+//     "Image": "https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg",
+//     "ReleaseDate": "2013-09-17",
+//     "Genres": [
+//       "Action"
+//     ],
+//     "Developer": "Rockstar North",
+//     "Screenshots": [
+//       "https://media.rawg.io/media/screenshots/f95/f9518b1d99210c0cae21fc09e95b4e31.jpg",
+//       "https://media.rawg.io/media/screenshots/a5c/a5c95ea539c87d5f538763e16e18fb99.jpg",
+//       "https://media.rawg.io/media/screenshots/a7e/a7e990bc574f4d34e03b5926361d1ee7.jpg",
+//       "https://media.rawg.io/media/screenshots/eb0/eb0a2c0de9194a635fc0cd04f5a29ae7.jpg",
+//       "https://media.rawg.io/media/screenshots/ec7/ec7f05fb82290fea9647b7879fe9a6bf.jpg"
+//     ],
+//     "Price": "17.43"
+//   },
+// ], ...
