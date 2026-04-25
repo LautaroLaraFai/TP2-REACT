2a94a0976fe9785701c8423308dc9382b570f553
Lautaro Lara <lautaronicolas.lara@est.fi.uncoma.edu.ar>
2026-04-25 17:24:26 -0300

    Lang stuff

--- src/i18n.js
+++ src/i18n.js
@@ -3,8 +3,9 @@ import { initReactI18next } from "react-i18next";
 
 import es from "../src/locales/es";
 import en from "../src/locales/en";
+import LANGUAGES from "../src/const/languages";
 
-//localStorage get item
+//localStorage.getItem("language", || LANGUAGES.es)
 
 i18n.use(initReactI18next).init({
     resources: {
