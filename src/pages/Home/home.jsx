e17a78441f52dda5bb022034d31f3f5132e26cb2
LucasLautaroMartinez <lucaslautaromartinez@gmail.com>
2026-04-27 15:16:54 -0300

    basic home

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,8 +1,62 @@
-const Home = () =>{
-    return <div>
-        Este es el home perros
-    </div>
-}
+import { useEffect, useState } from "react";
+import MainLayout from "../../layouts/MainLayout.jsx";
+import getData from "../../services/getData.js";
+import GameCardSmall from "../../components/GameCardSmall/GameCardSmall.jsx"
+import GameCardLarge from "../../components/GameCardLarge/GameCardLarge.jsx"
+
+export default function Home () {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    getData().then((data) => setGames(data)).catch(console.error);
+  }, []);
+
+  return (
+    <MainLayout>
+
+    {/* Principal recomendado */}
+    <section className="lg:px-20 md:px-14 sm:px-10 max-sm:px-6 pt-16 pb-20 max-sm:pt-12 max-sm:pb-16">
+      <GameCardLarge
+        key={"rellenar"}
+        description="Star Wars Jedi: Fallen Order es un videojuego de acción y aventuras ambientado dentro del universo ideado por George Lucas, concretamente entre los episodios"
+        image="https://static.wikia.nocookie.net/esstarwars/images/5/57/Fallen-Order-Box-Art.jpg/revision/latest/scale-to-width-down/1000?cb=20191020063836"
+        price="60.968,75"
+        name="Star Wars - The Fallen Order"
+        alt={""}
+        storeUrl="https://store.steampowered.com/..."
+        onClick={""}
+      />
+    </section>
 
 
-export default Home
+    {/* Todos los juegos */}
+    <section className="w-full pt-0">
+ 
+      <div className="border-t-2 border-a-amber">
+        <div className="inline-block md:px-36 sm:px-10 max-sm:px-6 py-4 max-sm:py-2 border-b-2 border-r-2 border-a-amber rounded-br-lg">
+          <h2 className="md:text-[1.8em] tracking-[.07em] text-a-amber">
+            Recomendaciones
+          </h2>
+        </div>
+      </div>
+ 
+      <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 py-10 lg:px-26 md:px-12 sm:px-10 max-sm:px-4">
+        {games.map((game) => (
+          <GameCardSmall
+            key={game.id}
+            image={game.Image}
+            price={game.Price}
+            name={game.Name}
+            alt={game.Name}
+            storeUrl="https://store.steampowered.com/..."
+            onClick={""}
+          />
+        ))}
+      </div>
+    </section>
+
+
+    
+    </MainLayout>
+  );
+};
