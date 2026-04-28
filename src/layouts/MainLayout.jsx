d9a6f9006281fe4af9695da475ed67ff3e5d64a8
Lautaro Lara <lautaronicolas.lara@est.fi.uncoma.edu.ar>
2026-04-28 14:20:41 -0300

    FIX/ translations and animations

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,11 +3,14 @@ import { Footer } from "../components/Footer/Footer.jsx";
 import Header from "../components/Header/Header.jsx";
 import GameCardSmall from "../components/GameCardSmall/GameCardSmall.jsx";
 import "../index.css"
+import { useTranslation } from "react-i18next";
 
 export default function MainLayout({ children, games = [] }) {
 
   const [searchActive, setSearchActive] = useState(false)
   const [filteredGames, setFilteredGames] = useState([])
+  
+  const { t } = useTranslation();
 
   return (
     <>
@@ -31,20 +34,30 @@ export default function MainLayout({ children, games = [] }) {
         {/* En caso de que se escriba en la searchBar */}
         {searchActive && (
           <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 py-10 lg:px-26 md:px-12 sm:px-10 max-sm:px-4">
-            {filteredGames.map((game) => (
-              <GameCardSmall
+            {filteredGames.map((game, index) => (
+              <div 
                 key={game.id}
-                image={game.Image}
-                price={game.Price}
-                name={game.Name}
-                alt={game.Name}
-                storeUrl="https://store.steampowered.com/..."
-                onClick={""}
-              />
+                className="animate-card"
+                style={{ animationDelay: `${index * 0.03}s` }}
+              >
+                <GameCardSmall
+                  image={game.Image}
+                  price={game.Price}
+                  name={game.Name}
+                  alt={game.Name}
+                  storeUrl="https://store.steampowered.com/..."
+                  onClick={""}
+                />
+              </div>
             ))}
+            {filteredGames.length === 0 && (
+              <div className="col-span-full text-center text-white text-2xl py-10">
+                {t("mainLayout.searchResultsText")}
+              </div>
+            )}
           </div>
         )}
-
+ 
         <Footer />
       </main>
     </>
