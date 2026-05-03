e7046066d083268037960ea65dc3d4e582ad25f2
LucasLautaroMartinez <lucaslautaromartinez@gmail.com>
2026-05-03 01:00:36 -0300

    feature/card-grid-favorite

---
+++ src/components/CardGridFavorite/CardGridFavorite.jsx
@@ -0,0 +1,23 @@
+import { useFavorite } from "../../hooks/useFavorite.jsx"
+import GameCardMedium from "../GameCardMedium/GameCardMedium.jsx";
+
+export const CardGridFavorite = ({ games, toggleFavorite, isFavorite }) => {
+
+  return (
+    <div className="flex flex-col gap-4 py-10 lg:px-26 md:px-12 sm:px-10 max-sm:px-4 md:mb-30 max-md:mb-20">
+      {games.map((game) => (
+        <GameCardMedium
+          key={game?.id}
+          gameId={game?.id}
+          name={game?.Name}
+          price={game?.Price}
+          image={game?.Image}
+          genres={game?.Genres}
+          releaseDate={game?.ReleaseDate}
+          onClick={() => toggleFavorite(game.id)}
+          isFavorite={isFavorite(game.id)} 
+        />
+      ))}
+    </div>
+  );
+};
