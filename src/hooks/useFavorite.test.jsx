4c334143de229b18cda51c58ce72b31790876294
Lautaro Lara <lautaronicolas.lara@est.fi.uncoma.edu.ar>
2026-05-16 18:31:08 -0300

    implemented testing for useFavorite

--- src/hooks/useFavorite.test.jsx
+++ src/hooks/useFavorite.test.jsx
@@ -1,5 +1,5 @@
-import { renderHook, act } from '@testing-library/react';
-import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { renderHook, act, render, waitFor } from '@testing-library/react';
+import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
 import useFavorite from './useFavorite';
 
 // Mock de localStorage
@@ -27,12 +27,55 @@ beforeEach(() => {
 
 describe('useFavorite', () => {
 
-    it('Initialization with empty array and loading false', () => {
+    it('Initialization with empty array', () => {
         const { result } = renderHook(() => useFavorite());
         
         expect(result.current.favoriteIds).toEqual([]);
         expect(result.current.loading).toBe(false);
     });
 
-    it()
+    it('Should return whether the item is in favorite or not', async () => {
+        localStorageMock.setItem('favorites', JSON.stringify(["1"]));
+        
+        const { result } = renderHook(() => useFavorite());
+        
+        await waitFor(() => {
+            expect(result.current.loading).toBe(false);
+        });
+        
+        expect(result.current.isFavorite("1")).toBe(true);
+        expect(result.current.isFavorite("3")).toBe(false);
+    });
+
+    it('Should add favorites to localStorage', () => {
+        const { result } = renderHook(() => useFavorite());
+        const { toggleFavorite } = result.current;
+
+        const initialStorage = JSON.parse(localStorage.getItem('favorites') || '[]');
+        expect(initialStorage).toEqual([]);
+
+        act(() => {
+            toggleFavorite("1");
+        });
+
+        const updatedStorage = JSON.parse(localStorage.getItem('favorites') || '[]');
+        expect(updatedStorage).toEqual([1]);
+    });
+
+    it('Should delete favorites', () => {
+        localStorage.setItem('favorites', JSON.stringify(["1", "5"]));
+        
+        const { result } = renderHook(() => useFavorite());
+        const { toggleFavorite } = result.current;
+
+        const initialStorage = JSON.parse(localStorage.getItem('favorites') || '[]');
+        expect(initialStorage).toEqual(["1", "5"]);
+
+        act(() => {
+            toggleFavorite("1");
+        });
+
+        const updatedStorage = JSON.parse(localStorage.getItem('favorites') || '[]');
+        expect(updatedStorage).toEqual([5]);
+    });
 });
